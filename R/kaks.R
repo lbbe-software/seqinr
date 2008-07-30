ab5eb315e612a22513f94559d426d37344d12331
simon.penel@univ-lyon1.fr <simon.penel@univ-lyon1.fr>
2008-07-30 16:14:25 +0000

    Addition of last release R contents

---
+++ R/kaks.R
@@ -0,0 +1,68 @@
+kaks <- function(x, debug = FALSE, forceUpperCase = TRUE){
+    #
+    # Check argument class:
+    #
+    if(attr(x,"class") != "alignment") stop("object x must be of class alignment")
+    if(debug){
+      cat("<--- Argument x storage is --->\n")
+      print(str(x))
+      cat("<--- Argument x storage is --->\n")
+    }
+    #
+    # Check that there are at least two sequences in the alignment:
+    #
+    if(x$nb < 2){
+      warning("there should be at least two sequences in the alignment")
+      return(NA)
+    }
+    #
+    # Check that all sequences are of the same length:
+    #
+    lseqs <- nchar(x$seq)
+    if( !all(lseqs == lseqs[1])) {
+      warning("all sequences should be the same length in an alignment")
+      return(NA)
+    }
+    #
+    # Check that the length of sequences is a mutiple of 3 since we are dealing
+    # with coding sequences here:
+    #
+    if( lseqs[1] %% 3 != 0){
+      warning("sequence lengths are not a multiple of 3")
+      return(NA)
+    }
+    #
+    # Force sequences characters to upper case letters when at least one
+    # one 'a', 'c', 'g', or 't' is found in the sequences:
+    #
+    if(forceUpperCase){
+      if( length(grep("[acgt]", x$seq)) != 0){
+        x$seq <- toupper(x$seq)
+      }
+    }
+    #
+    # Call internal C function:
+    #
+    l <- .Call("kaks", x$seq, x$nb, debug, PACKAGE = "seqinr")
+    if(debug){
+      cat("<--- Result l storage is --->\n")
+      print(str(l))
+      cat("<--- Result l storage is --->\n")
+    }
+    #
+    # If the sequences names are missing, we call them seq1, seq2, and so on:
+    #
+    if( is.null(x$nam) ) x$nam <- paste("seq", seq_len(x$nb), sep = "")
+    
+    #
+    # This is to compute the list of results:
+    #
+    mkresult <- function(k){
+      tmp <- matrix( k, x$nb, x$nb, byrow = TRUE, dimnames = list(x$nam, x$nam))
+      as.dist(t(tmp))
+    }
+    result <- lapply(l[seq_len(4)], mkresult)
+    names(result) <- c("ka", "ks", "vka", "vks")
+    return(result)
+}
+
---
+++ R/words.pos.R
@@ -0,0 +1,14 @@
+words.pos <- function(pattern, text, ignore.case = FALSE, extended = FALSE,
+                      perl = TRUE, fixed = FALSE, useBytes = TRUE, ...)
+{
+  position <- regexpr(pattern, text, ignore.case, extended, perl, fixed, useBytes, ...)[1]
+  result <- numeric(0)
+  while(position != -1 )
+  {
+    result <- c(result, position )
+    text <- substr(text, position + 1, nchar(text))
+    position <- regexpr(pattern, text, ignore.case, extended, perl, fixed, useBytes, ...)[1]
+  }
+  return(cumsum(result))
+}
+
