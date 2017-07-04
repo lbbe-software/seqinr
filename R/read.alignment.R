ff2ff35401d09df39380b48084777feeee1a96d9
jean.lobry@univ-lyon1.fr <jean.lobry@univ-lyon1.fr>
2017-07-04 10:25:33 +0000

    read.alignment can now handle legacy fasta format

--- R/read.alignment.R
+++ R/read.alignment.R
@@ -9,18 +9,17 @@ read.alignment <- function(file, format, forceToLower = TRUE)
   file <- path.expand(file) 
   if(file.access(file, mode = 4) != 0) stop(paste("File", file, "is not readable"))
   
-  ali <- switch( format,
-	fasta = .Call("read_fasta_align", file, PACKAGE = "seqinr"), 
-	FASTA = .Call("read_fasta_align", file, PACKAGE = "seqinr"), 
+  fasta2ali <- function(file){
+  	tmp <- read.fasta(file, as.string = TRUE)
+  	list(length(tmp), getName(tmp), unlist(getSequence(tmp, as.string = TRUE)))
+  }
+  ali <- switch( tolower(format),
+	fasta = fasta2ali(file), 
 	mase = .Call("read_mase", file, PACKAGE = "seqinr"),
-	MASE = .Call("read_mase", file, PACKAGE = "seqinr"),
 	phylip = .Call("read_phylip_align", file, PACKAGE = "seqinr"),
-	PHYLIP = .Call("read_phylip_align", file, PACKAGE = "seqinr"),
 	msf = .Call("read_msf_align", file, PACKAGE = "seqinr"),
-	MSF = .Call("read_msf_align", file, PACKAGE = "seqinr"),
-	CLUSTAL = .Call("read_clustal_align", file, PACKAGE = "seqinr"),
 	clustal = .Call("read_clustal_align", file, PACKAGE = "seqinr"),
-	stop("Wrong format name: Format available are fasta,mase,phylip,msf,clustal")
+	stop("Wrong format name: Format available are fasta, mase, phylip, msf, clustal")
   )
 
   ali <- lapply(ali, as.character)
